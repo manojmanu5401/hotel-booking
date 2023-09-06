bd1702526e41a88462fdf1d853f371fa93e51c34
Manoj M <manojmanu5401@gmail.com>
2023-09-06 10:01:29 +0530

    mobile view filter

--- src/components/HotelsList.jsx
+++ src/components/HotelsList.jsx
@@ -202,63 +202,18 @@ const HotelsList = () => {
                     </div>
 
                     {/* Filters */}
-                    <form className="mt-10 border-t border-gray-200">
+                    <form className="mt-10 border-t border-gray-200 p-5">
                       <h3 className="sr-only">Categories</h3>
-                      {/* {filters.map((section) => (
-                        <Disclosure
-                          as="div"
-                          key={section.id}
-                          className="border-t border-gray-200 px-4 py-6"
-                        >
-                          {({ open }) => (
-                            <>
-                              <h3 className="-mx-2 -my-3 flow-root">
-                                <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                  <span className="font-medium text-gray-900">
-                                    {section.name}
-                                  </span>
-                                  <span className="ml-6 flex items-center">
-                                    {open ? (
-                                      <span className="material-symbols-outlined h-5 w-5">
-                                        remove
-                                      </span>
-                                    ) : (
-                                      <span className="material-symbols-outlined h-5 w-5">
-                                        add
-                                      </span>
-                                    )}
-                                  </span>
-                                </Disclosure.Button>
-                              </h3>
-                              <Disclosure.Panel className="pt-6">
-                                <div className="space-y-6">
-                                  {section.options.map((option, optionIdx) => (
-                                    <div
-                                      key={option.value}
-                                      className="flex items-center"
-                                    >
-                                      <input
-                                        id={`filter-mobile-${section.id}-${optionIdx}`}
-                                        name={`${section.id}[]`}
-                                        defaultValue={option.value}
-                                        type="checkbox"
-                                        defaultChecked={option.checked}
-                                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                      />
-                                      <label
-                                        htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                        className="ml-3 min-w-0 flex-1 text-gray-500"
-                                      >
-                                        {option.label}
-                                      </label>
-                                    </div>
-                                  ))}
-                                </div>
-                              </Disclosure.Panel>
-                            </>
-                          )}
-                        </Disclosure>
-                      ))} */}
+                      <Filter
+                      options={priceFilter}
+                      handleMethod={handlePriceFilter}
+                      title="Price per Night"
+                    />
+                    <Filter
+                      options={starFilter}
+                      handleMethod={handleStarFilter}
+                      title={"Star Catogery"}
+                    />
                     </form>
                   </Dialog.Panel>
                 </Transition.Child>
